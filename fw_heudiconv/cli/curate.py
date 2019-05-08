a32eea1d2cf2b0ec4a0981f95a34b0cc83ba58d8
TinasheMTapera <teetaps@yahoo.com>
2019-05-08 14:11:19 -0400

    bugfix

--- fw_heudiconv/cli/curate.py
+++ fw_heudiconv/cli/curate.py
@@ -84,8 +84,15 @@ def convert_to_bids(client, project_label, heuristic_path, subject_labels=None,
 
     if not dry_run:
         logger.info("Applying changes to files...")
-    subject_rename = heuristic.get("ReplaceSubject")
-    session_rename = heuristic.get("ReplaceSession")
+
+    if hasattr(heuristic, "ReplaceSubject"):
+        subject_rename = heuristic.ReplaceSubject
+    else:
+        subject_rename = None
+    if hasattr(heuristic, "ReplaceSession"):
+        session_rename = heuristic.ReplaceSession
+    else:
+        session_rename = None
 
     for key, val in to_rename.items():
         apply_heuristic(client, key, val, dry_run, intention_map[key],
