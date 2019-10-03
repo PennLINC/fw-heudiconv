12e137b8f7c6cf3d62163e3e0db371a80acbf7fa
TinasheMTapera <teetaps@yahoo.com>
2019-10-03 10:54:05 -0400

    bugfix #48

--- fw_heudiconv/query.py
+++ fw_heudiconv/query.py
@@ -104,7 +104,7 @@ def session_to_seq_info(client, session, context):
     seq_info = collections.OrderedDict()
     context['total'] = 0
     acquisitions = session.acquisitions()
-    sorted_acquisitions = sorted(acquisitions, key=lambda x: x.timestamp)
+    sorted_acquisitions = sorted(acquisitions, key=lambda x: x.timestamp or '')
     for acquisition in sorted_acquisitions:
         acquisition = client.get(acquisition.id)
         context['acquisition'] = acquisition
