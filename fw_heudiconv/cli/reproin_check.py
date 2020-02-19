7061ce8a29ee1e62f3d3363e8448fa5dd4cbb0d1
TinasheMTapera <teetaps@yahoo.com>
2020-02-19 12:08:16 -0500

    fixed regex

--- fw_heudiconv/cli/reproin_check.py
+++ fw_heudiconv/cli/reproin_check.py
@@ -13,20 +13,22 @@ logger = logging.getLogger('fw-heudiconv-reproin')
 
 
 def check(string, verbose):
-
+    '''
+    https://regex101.com/r/w0SRcs/1/
+    '''
     assert isinstance(string, str), "Input is not a string!"
 
     result = re.search(
-        r'^([A-Z]+:)?' # PREFIX
-        '((anat)|(func)|(fmap)|(dwi))-([a-zA-Z0-9]+){1}' # Seqtype & label
-        '(_ses-[a-zA-Z0-9]+)?' # optional session
-        '(_task-[a-zA-Z0-9]+)?' # task
-        '(_acq-[a-zA-Z0-9]+)?' # acquisition
-        '(_ce-[a-zA-Z0-9]+)?' # contrast
-        '(_dir-[a-zA-Z0-9]+)?' # directionality
-        '(_rec-[a-zA-Z0-9]+)?' # reconstruction
-        '(_run-[a-zA-Z0-9]+)?' # runs
-        '(__.*)$', # dunder and custom info
+        r'^([A-Z]+:)?'
+        '((anat)|(func)|(fmap)|(dwi))-([\w]+){1}'
+        '(_ses-[a-zA-Z0-9]+)?'
+        '(_task-[a-zA-Z0-9]+)?'
+        '(_acq-[a-zA-Z0-9]+)?'
+        '(_ce-[a-zA-Z0-9]+)?'
+        '(_dir-[a-zA-Z0-9]+)?'
+        '(_rec-[a-zA-Z0-9]+)?'
+        '(_run-[a-zA-Z0-9]+)?'
+        '(__.*)?$',
         string)
 
     if result:
