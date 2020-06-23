74abdadbcbd56ffd0040afe3955d4c2cd34ecbf0
TinasheMTapera <teetaps@yahoo.com>
2020-06-23 07:30:55 -0400

    added functionality to tabulate the errors and warnings generated by the validator

--- fw_heudiconv/cli/validate.py
+++ fw_heudiconv/cli/validate.py
@@ -1,17 +1,36 @@
 import os
 import sys
 import shutil
-import subprocess as sub
 import logging
 import argparse
 import warnings
+import re
 from pathlib import Path
+import subprocess as sub
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('fw-heudiconv-validator')
 
 
-def validate_local(path, verbose):
+def escape_ansi(line):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', line)
+
+
+def find_all(regex, text):
+
+    match_list = []
+    while True:
+        match  = re.search(regex, text)
+        if match:
+            match_list.append(match.group(0))
+            text = text[match.end():]
+        else:
+            return match_list
+
+
+def validate_local(path, verbose, tabulate=True):
 
     logger.info("Launching bids-validator...")
     command = ['bids-validator', path]
@@ -21,8 +40,38 @@ def validate_local(path, verbose):
     output, error = p.communicate()
 
     logger.info(output)
+
     if p.returncode != 0:
         logger.info(error)
+
+    clean_output = re.sub('\s+',' ', escape_ansi(output))
+    issues = find_all(r'\[[A-Z]+\].+?(?=(\[[A-Z]+\]|Summary))', clean_output)
+
+    if issues and tabulate:
+
+        logger.info("Parsing issues and writing to issues.csv")
+        issues_tuples = []
+
+        for iss in issues:
+            issue_type_match = re.search(r'\[[A-Z]+\]', iss)
+            issue_type = iss[:issue_type_match.end()]
+            issue_description = iss[issue_type_match.end():].lstrip()
+            issue_evidence = find_all(
+                r'(?<=Evidence: ).+?(?=(Evidence|\[[A-Z]+\]))', issue_description
+                )
+            if issue_evidence:
+                evidence_ix = re.search('Evidence', issue_description).start()
+                issue_description = issue_description[:evidence_ix]
+            else:
+                issue_evidence = ''
+
+            issues_tuples.append((issue_type, issue_description, issue_evidence))
+
+        issues_df = pd.DataFrame(issues_tuples, columns=['Type', 'Description', 'Evidence'])
+        issues_df_full = issues_df.explode('Evidence')
+        logger.info("Writing...")
+        issues_df_full.to_csv('./issues.csv', index=False)
+
     return p.returncode
 
 
@@ -65,7 +114,7 @@ def get_parser():
     parser.add_argument(
         "--directory",
         help="Path to existing BIDS data directory OR temp space used for validation",
-        default="bids_directory",
+        default=".",
         required=False,
         type=str
     )
@@ -92,6 +141,12 @@ def get_parser():
         "--verbose",
         help="Pass on <VERBOSE> flag to bids-validator",
         default=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        "--tabulate",
+        help="Tabulate errors and save to file for quicker analysis",
+        default=True,
         action='store_true'
     )
     parser.add_argument(
@@ -132,15 +187,15 @@ def main():
             logger.error("No project on Flywheel specified!")
             sys.exit(exit)
 
-        if not os.path.exists(args.directory):
-            logger.info("Creating download directory...")
-            os.makedirs(args.directory)
+        #if not os.path.exists(args.directory):
+        #    logger.info("Creating download directory...")
+        #    os.makedirs(args.directory)
 
         success = fw_heudiconv_export(proj=args.project, subjects=args.subject, sessions=args.session, destination=args.directory, name='bids_directory', key=args.api_key)
 
         if success == 0:
-            path = Path(args.directory + '/bids_directory')
-            exit = validate_local(path, args.verbose)
+            path = Path(args.directory, 'bids_directory')
+            exit = validate_local(path, args.verbose, args.tabulate)
             shutil.rmtree(path)
 
         else:
