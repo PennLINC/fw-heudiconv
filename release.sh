2d1ec249fad42d57b8b965a01f3604bd1dbdf197
TinasheMTapera <tinashemtapera@gmail.com>
2021-05-12 13:06:59 -0400

    added bash script to automate releases

---
+++ release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No version argument supplied"
+    exit
+fi
+
+echo Upgrading and uploading fw-heudiconv...
+
+VERSION=$1
+
+echo Building docker image:
+echo docker build -t pennbbl/fw-heudiconv:$VERSION .
+
+docker build -t pennbbl/fw-heudiconv:$VERSION .
+
+echo Testing local help call...
+
+fw gear local --help
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error with local test"
+    exit $retVal
+fi
+
+echo Uploading to pip...
+
+python setup.py sdist
+twine upload dist/*
+
+echo Uploading gear...
+
+fw gear upload
